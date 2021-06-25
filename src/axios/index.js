3d55708e7d364496ca454dd1878ba8ff68fd480f
Shubham <bakhalshubham@gmail.com>
2021-06-25 18:42:28 +0530

    axios remote link

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  // baseURL: 'https://class-calender.herokuapp.com/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://class-calender.herokuapp.com/api',
 });
 axiosInstance.defaults.withCredentials = true;
 export default axiosInstance;
